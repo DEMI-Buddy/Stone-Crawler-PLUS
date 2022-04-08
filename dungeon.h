29bab3694aaa767b920280f271e418780c78999a
DEMI-Buddy <100232650+DEMI-Buddy@users.noreply.github.com>
2022-04-07 19:04:18 -0500

    Add files via upload

--- dungeon.h
+++ dungeon.h
@@ -59,6 +59,9 @@ class dungeon_crawling : public system_handler
 				cout << "ERROR, DUNGEON MAP NOT FOUND";
 			
 			fileload.close();
+			
+			newGame = true;
+			loadIn = true;
 		}
 		
 		// display dungeon 
@@ -74,46 +77,109 @@ class dungeon_crawling : public system_handler
 				for(int x=max_x-1;x>=0;x--)
 				{
 					// display specific block
-					renderRect = {0+40*((map[pZ][y][x]-1)%2),0,40,40};
-							
+					renderRect = {40*(map[pZ][y][x]-'0'),0,40,40};
+					
 					if((pY-1 == y || pY == y || pY+1 ==y) && (pX-1 == x || pX == x || pX+1 ==x))
 						brick.setColor(255,255,255);
 					else
 						brick.setColor(100,100,100);
 					
 					brick.render(main_game->renderer,cameraX+(y*20*scale)+(x*20*scale),cameraY-(x*10*scale)+(y*10*scale),&renderRect);
+						
 										
 					// display player 
-					player.render(main_game->renderer,cameraX+(pY*20*scale)+(pX*20*scale),cameraY-(pX*10*scale)+(pZ*20*scale)+(pY*10*scale)-25*scale);
-					
+					if(!loadIn && !switchOut)
+						player.render(main_game->renderer,cameraX+(pY*20*scale)+(pX*20*scale),cameraY-(pX*10*scale)+(pY*10*scale)-25*scale);
+					else
+					{
+						brick.setAlpha(megaAlpha);
+						player.render(main_game->renderer,cameraX+(pY*20*scale)+(pX*20*scale),cameraY-(pX*10*scale)+(pY*10*scale)-25*scale + moveAnimation);					
+						if(!newGame)
+						{
+							switch(up)
+							{
+								case true:
+								moveAnimation-=1;
+								break;
+								case false:
+								moveAnimation+=1;
+								break;
+							}
+						}
+					}
 				}	
 			}	
+			
+			if(loadIn && megaAlpha == 255)
+			{
+				loadIn = false;
+				newGame = false;
+			}
+			else if(loadIn)
+				megaAlpha+=5;
+			else if(switchOut && megaAlpha == 0)
+			{
+				megaAlpha = 255;
+				switchOut = false;
+				loadIn = true;
+			}
+			else if(switchOut)
+				megaAlpha-=5;
+			
+			
 		}
 		
 		// input handling
 		void handler() override
 		{
-			switch(main_game->input.state)
+			if(!loadIn && !switchOut)
 			{
-				case UP:
-				if((pY != 0) && (map[pZ][pY-1][pX] == '1'))
-					pY--;
-				break;
-				case LEFT:
-				if((pX != 0) && (map[pZ][pY][pX-1] == '1'))
-					pX--;
-				break;
-				case RIGHT:
-				if((pX != max_x) && (map[pZ][pY][pX+1] == '1'))
-					pX++;
-				break;
-				case DOWN:
-				if((pY != max_y-1) && (map[pZ][pY+1][pX] == '1'))
-					pY++;
-				break;
+				switch(main_game->input.state)
+				{
+					case UP:
+					if((pY != 1) && (map[pZ][pY-1][pX] != '0'))
+						pY--;
+					break;
+					case LEFT:
+					if((pX != 0) && (map[pZ][pY][pX-1] != '0'))
+						pX--;
+					break;
+					case RIGHT:
+					if((pX != max_x-1) && (map[pZ][pY][pX+1] != '0'))
+						pX++;
+					break;
+					case DOWN:
+					if((pY != max_y-1) && (map[pZ][pY+1][pX] != '0'))
+						pY++;
+					break;
+				}
+				switch(map[pZ][pY][pX])
+				{
+					case '2':
+					up = true;
+					switchOut = true;
+					pZ++;
+					moveAnimation = 0;
+					break;
+					case '3':
+					up = false;
+					switchOut = true;
+					pZ--;
+				    moveAnimation = 0;
+					break;
+				}
 			}
 		}
 	private:
+		// variables used for loading in a new floor 
+		bool loadIn = false;
+		bool switchOut = false;
+		int megaAlpha = 0;
+		
+		int moveAnimation = 0;
+		bool newGame = false;
+		bool up = true;
+		
 		// for rendering specific blocks from the block image 
 		SDL_Rect renderRect = {0,0,40,40};
 		
