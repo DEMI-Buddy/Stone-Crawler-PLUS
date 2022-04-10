5d7b2bd2ce10946294ac2bdf2fe3fcbb334b7963
DEMI-Buddy <100232650+DEMI-Buddy@users.noreply.github.com>
2022-04-10 12:35:29 -0500

    Add files via upload

--- battle.h
+++ battle.h
@@ -5,6 +5,39 @@ class battle : public system_handler
 	public:
 		battle(){}
 		
+		// adding new description dialogue for battle text 
+		void actionLine(string command)
+		{
+			if(command.size() > 30)
+			{
+				lines.push_back(command.substr(0,30));
+				lines.push_back(command.substr(30));
+			}
+			else
+				lines.push_back(command);
+			currentScript++;
+			texttimer.start();
+			currentPos = 0;
+			finishedLine = false;
+		}
+		
+		// generating/displaying battle text information
+		void textInfo()
+		{
+			counter = (int)(16*texttimer.getTicks()/1000.f);		
+			if(counter > currentPos && currentPos < lines[currentScript].size())
+				currentPos++;
+			
+			// checks if text is done 
+			if(currentPos >= lines[currentScript].size() && currentScript == lines.size()-1)
+			{
+				finishedLine = true;
+				texttimer.stop();
+			}
+			
+			combat_info.display(lines[currentScript].substr(0,currentPos),70,50);		
+		}
+		
 		battle(game_handler * g)
 		{
 			main_game = g;
@@ -22,6 +55,8 @@ class battle : public system_handler
 			enemies = new image("resources/sprites/Monster/007.png",g->renderer);
 			numTypeEnemies = 1;
 			
+			endHeart = image("resources/sprites/heart.png",g->renderer);
+			
 			player_port = image("resources/sprites/player_portrait.png",g->renderer);
 			
 			for(int i=0;i<numTypeEnemies;i++)
@@ -31,8 +66,12 @@ class battle : public system_handler
 			border.scale = 2;
 			party_menu.scale = 2;
 			area.scale = 2;
+			
+			texttimer.start();
+			
+			lines.push_back("Encountered a Stranjger!");
 		}
-		
+
 		// display battle 
 		void display() override
 		{
@@ -76,46 +115,97 @@ class battle : public system_handler
 				if(i==0)
 					player_port.render(main_game->renderer,600,140);
 				
-				main_game->displayText.display("HP:10/10",730,160);
-				main_game->displayText.display("PLAYER",750,220);
+				combat_info.display("HP:10/10",750,160);
+				combat_info.display("STAMINA:10",750,205);
+				combat_info.display("PLAYER",750,250);
 			
 			}
 			textArea.render(main_game->renderer,20,25);
-			combat_info.display("TESTING",70,50);
+			textInfo();
 			
+			if(finishedLine)
+			{
+				endHeart.render(main_game->renderer,600,95+pointy);
+				
+				// end heart up and down animation 
+				if(pointy > 5)
+					reverse = true;
+				else if(pointy < -5)
+					reverse = false;
+			
+				if(reverse)
+					pointy-=1;
+				else
+					pointy+=1;
+			}
 		}		
 	
 		// handle input/logic
 		void handler() override
 		{
-			switch(main_game->input.state)
+			if(finishedLine)
 			{
-				case DOWN:
-				if(option != 3)
-					option++;
-				break;
-				case UP:
-				if(option != 0)
-					option--;
-				break;
+				switch(main_game->input.state)
+				{
+					case DOWN:
+					if(option != 3)
+						option++;
+					break;
+					case UP:
+					if(option != 0)
+						option--;
+					break;
+					case SELECT:
+					actionLine("You did something!");
+					break;
+				}
 			}
 		}
 	private:
-		// for displaying info on what's happening in battle  
-		text combat_info; 
+		//--------------image rendering variables--------------
+			// hold images of enemy sprites
+			image * enemies; 
+			// the number of types of enemies used for the enemy sprite pointer
+			int numTypeEnemies;
 		
-		// hold images of enemy sprites
-		image * enemies; 
-		// the number of types of enemies used for the enemy sprite pointer
-		int numTypeEnemies;
+			// images used for UI
+			image textArea;
+			image menu;
+			image party_menu;
+			image player_port;
+			image border;
+			image area;
+			image endHeart;
 		
-		image textArea;
-		image menu;
-		image party_menu;
-		image player_port;
-		int option = 0;
-		int partySize = 1;
+			// the option the player selects for combat 
+			int option = 0;
 		
-		image border;
-		image area;
+			// animation variables for movement of heart at the end of line 
+			double pointy = 0;
+			bool reverse = true;
+		
+		//--------------text rendering variables--------------			
+			// for displaying info on what's happening in battle  
+			text combat_info; 
+			
+			// timer for keeping track of speed for displaying tex	t
+			timer texttimer; 
+		
+			// rate variable for showing characters 
+			int counter = 0;
+		
+			// current character in the string and the counter for what is displayed
+			int currentPos = 0;
+			
+			// the text being displayed
+			vector<string> lines = vector<string>();
+			
+			// current line being read
+			int currentScript = 0;
+			
+			bool finishedLine = false;
+		
+		//--------------testing/debugging variables--------------		
+			// the party size (for debugging)
+			int partySize = 1;
 };
